32be68760e4c7b7df20ddff0f18e93e416ff5148
task <pavelevl@mail.ru>
2024-11-24 09:47:49 +0700

    Д\З 2.0 с ломбоком

--- innopolis/src/main/java/Leonid/Product.java
+++ innopolis/src/main/java/Leonid/Product.java
@@ -1,7 +1,13 @@
 package Leonid;
 
-import java.util.UUID;
+import lombok.*;
 
+import java.util.UUID;
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     private UUID id;
@@ -9,56 +15,5 @@ public class Product {
     private double price;
     private int count;
 
-    public Product() {
-    }
-
-    public Product(UUID id, String description, double price, int count) {
-        this.id = id;
-        this.description = description;
-        this.price = price;
-        this.count = count;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                ", count=" + count +
-                '}';
-    }
 }
 
