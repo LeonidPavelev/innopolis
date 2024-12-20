95ff7b08f9fb8121996d7cf4ab7c424f527fcb60
l.pavelev <pavelevl@mail.ru>
2024-12-20 15:31:27 +0300

    Д\З 4.0

---
+++ innopolis/src/main/java/leonid/InnopolisApplication.java
@@ -0,0 +1,11 @@
+package leonid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnopolisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InnopolisApplication.class, args);
+    }
+}
